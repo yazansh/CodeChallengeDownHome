81b57e333bd4076a16200efacaa0618f809f5a5b
yazansh <yazan.shakhshir@gmail.com>
2019-06-17 20:42:50 +0300

    second version

--- Program.cs
+++ Program.cs
@@ -11,7 +11,10 @@ namespace DownHomeCodeChallenge
     {
         static void Main(string[] args)
         {
-            string[] input = File.ReadAllLines(@"C:\\Users\\Msys\\Desktop\\Downhome Code Challenge\\Input.txt"); // reads all input string lines from Input.txt
+            Console.WriteLine("Pleae enter the path of the folder that contains Input.txt file");
+            String path = Console.ReadLine();
+
+            string[] input = File.ReadAllLines(@path+"\\Input.txt"); // reads all input string lines from Input.txt
             string[] output = new string[input.Length];
 
             for (int i = 0; i < input.Length; i++)
@@ -20,7 +23,7 @@ namespace DownHomeCodeChallenge
                 output[i] = ConvertToString(asciiNumbers); // convert integer array to string and add the string to Output Array of strings
             }
 
-            File.WriteAllLines(@"C:\\Users\\Msys\\Desktop\\Downhome Code Challenge\\my-outupt.txt",output); // writes the output to my-outpu.txt
+            File.WriteAllLines(path+"\\my-outupt.txt",output); // writes the output to my-outpu.txt
             
             Console.ReadLine();
         }
@@ -34,21 +37,61 @@ namespace DownHomeCodeChallenge
             return line;
         }
 
+        public static bool IsDigit(int c)
+        {
+            if (c >= 48 && c <= 57)
+                return true;
+            else
+                return false;
+        }
+
+        /* This method detects every partition/sequence then calls the partialSort method to sort this certain sequence.
+         * it checks every char whether it is a digit or not using the IsDigit method.
+         * used (leftIndex) and (rightIndex) to detect sequence limits.
+         * */
         private static int[] SortString(string input)
         {
             int[] asciiNumbers = new int[input.Length];
+            int leftIndex = 0;
+            int rightIndex = 0;
+            bool lastCharType = IsDigit(input[0]); // if char is digit it will have the value "True" else the value is "False".
+            asciiNumbers[0] = input[0];
+            bool currentCharType = false;
 
-            for (int i = 0; i < input.Length; i++)
+            for (int i = 1; i < input.Length; i++)
+            {
                 asciiNumbers[i] = input[i];
+                currentCharType = IsDigit(asciiNumbers[i]);
 
-            PartialSort(asciiNumbers);
+                if (currentCharType != lastCharType)
+                {
+                    PartialSort(asciiNumbers, leftIndex, rightIndex);
+                    leftIndex = i;
+                }
 
+                rightIndex = i;
+                lastCharType = currentCharType;
+            }
+
+            PartialSort(asciiNumbers, leftIndex, rightIndex);
             return asciiNumbers;
         }
 
-        public static void PartialSort(int[] array)
+        /* This method operates on the sequence sent by SortString method.
+         * it sorts the sequence then assign the values back to the original array in the new order.
+         */
+        public static void PartialSort(int[] array, int leftIndex, int rightIndex)
         {
-            Array.Sort(array);
+            int[] tempArray = new int[rightIndex - leftIndex + 1];
+            for (int j = 0, i = leftIndex; i <= rightIndex; i++, j++)
+                tempArray[j] = array[i];
+
+            Array.Sort(tempArray);
+            
+            for (int j = 0, i = leftIndex; i <= rightIndex; i++, j++)
+                array[i] = tempArray[j];
+
+            return;
         }
 
     }
